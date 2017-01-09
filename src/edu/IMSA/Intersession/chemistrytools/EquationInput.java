19cce3b66301d655dfb6e98e2ff649121735ece3
ikethefifth <mwestern@imsa.edu>
2017-01-09 14:59:07 -0600

    Initial GUI
    Most basic. Just make a new object for future use

---
+++ src/edu/IMSA/Intersession/chemistrytools/EquationInput.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.IMSA.Intersession.chemistrytools;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+/**
+ *
+ * @author student
+ */
+public class EquationInput {
+    public static void main(String[] args){
+        EquationInput window = new EquationInput();
+    }
+}
