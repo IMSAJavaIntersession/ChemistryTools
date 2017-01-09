f4b5ffcc68cfcf3bb4c1cfaf50eb571ea4fbb729
cschlegel1 <cschlegel@imsa.edu>
2017-01-09 15:49:26 -0600

    Added symbol array

--- src/edu/IMSA/Intersession/chemistrytools/PeriodicTable.java
+++ src/edu/IMSA/Intersession/chemistrytools/PeriodicTable.java
@@ -15,5 +15,10 @@ public class PeriodicTable {
         elm[0] = "Hydrogen";
         elm[1] = "Helium";
         elm[2] = "Lithium";
+        
+        String[] sym = new String[3];
+        sym[0] = "H";
+        sym[1] = "He";
+        sym[2] = "Li";
     }
 }
