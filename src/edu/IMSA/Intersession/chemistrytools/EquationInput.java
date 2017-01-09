d94f48fa9eedf04012e764274dd9f75506dc6986
ikethefifth <mwestern@imsa.edu>
2017-01-09 15:28:18 -0600

    Window
    We added a window. It's red.

--- src/edu/IMSA/Intersession/chemistrytools/EquationInput.java
+++ src/edu/IMSA/Intersession/chemistrytools/EquationInput.java
@@ -4,17 +4,35 @@
  * and open the template in the editor.
  */
 package edu.IMSA.Intersession.chemistrytools;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 /**
  *
  * @author student
  */
-public class EquationInput {
+public class EquationInput implements ActionListener {
+    JButton butt;
     public static void main(String[] args){
         EquationInput window = new EquationInput();
+        window.setup();
+    }
+    public void setup(){
+        butt = new JButton("Exit");
+        JFrame frame = new JFrame("Window");
+        JPanel panel = new JPanel();
+        butt.addActionListener(this);
+        panel.add(butt);
+        Dimension d = new Dimension(500, 300);
+        frame.setPreferredSize(d);
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+        frame.pack();
+        panel.setBackground(Color.RED);
+    }
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==butt){
+            System.exit(0);
+        }
     }
 }
