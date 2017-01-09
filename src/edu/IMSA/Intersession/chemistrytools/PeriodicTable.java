d59dc8090ea59af61e740cd730ffff6a219adab5
cschlegel1 <cschlegel@imsa.edu>
2017-01-09 15:55:23 -0600

    Fixed Mistakes

--- src/edu/IMSA/Intersession/chemistrytools/PeriodicTable.java
+++ src/edu/IMSA/Intersession/chemistrytools/PeriodicTable.java
@@ -10,15 +10,19 @@ package edu.IMSA.Intersession.chemistrytools;
  * @author student
  */
 public class PeriodicTable {
+    
+    String[] elm = new String[3];
+    String[] sym = new String[3];
+    
     public PeriodicTable(){
-        String[] elm = new String[3];
+        
         elm[0] = "Hydrogen";
         elm[1] = "Helium";
         elm[2] = "Lithium";
         
-        String[] sym = new String[3];
         sym[0] = "H";
         sym[1] = "He";
         sym[2] = "Li";
+        
     }
 }
