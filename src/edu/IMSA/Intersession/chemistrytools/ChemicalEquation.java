ca88e1d5d1f8080f9e3bc48173338782ee7ad980
SutterButter4 <jsutter909@gmail.com>
2017-01-09 15:29:35 -0600

    Split Equation into two parts
    looks for hyphen in equation and splits at that hyphen to form two
    strings

--- src/edu/IMSA/Intersession/chemistrytools/ChemicalEquation.java
+++ src/edu/IMSA/Intersession/chemistrytools/ChemicalEquation.java
@@ -1,6 +1,34 @@
-
 package edu.IMSA.Intersession.chemistrytools;
 
+import java.util.*;
+
 public class ChemicalEquation {
+    String equation;
     
+    String equationSide1;
+    String equationSide2;
+    
+    ArrayList moleculeList = new ArrayList();
+    
+    public ChemicalEquation(String inEquation)
+    {
+        equation = inEquation;
+    }
+    
+    public void seperateSides()
+    {
+        String[] parts = equation.split("-");
+        equationSide1 = parts[0];
+        equationSide2 = parts[1];
+    }
+    
+    public void seperateMolecules()
+    {
+        
+    }
+    
+    public ArrayList getMolecules()
+    {
+        return moleculeList;
+    }
 }
