21951a88e78c30f3b5ae103893b531db79124793
lrdemolition <dr.lrdemolition@gmail.com>
2017-01-10 15:44:09 -0600

    fixed

--- src/edu/IMSA/Intersession/chemistrytools/PeriodicTable.java
+++ src/edu/IMSA/Intersession/chemistrytools/PeriodicTable.java
@@ -10,6 +10,7 @@ package edu.IMSA.Intersession.chemistrytools;
  * @author student
  */
 public class PeriodicTable { 
+    int n;
     
     Element[] elm = new Element[3];
     //String[] sym = new String[3];
@@ -21,12 +22,13 @@ public class PeriodicTable {
         elm[0] = new Element("Oxygen", 9, "O",15.9);
     }
     public Element getElement(String symbol){
-        Element e;
+        int n = 9000;
         for(int i = 0; i<1; i++){
             if (elm[i].getSymbol() == symbol){
-                e = elm[i];
+                
+                n=i;
             }
         }
-        return e;
+        return elm[n];
     }
 }
