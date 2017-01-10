a58529e5d596a34c785682212dd8c2746ce9b764
AX20160802\student <anniexu2001@gmail.com>
2017-01-09 20:48:56 -0600

    MolecularWeight

--- src/edu/IMSA/Intersession/chemistrytools/MolecularFormula.java
+++ src/edu/IMSA/Intersession/chemistrytools/MolecularFormula.java
@@ -8,7 +8,7 @@ package edu.IMSA.Intersession.chemistrytools;
 import java.util.*;
 public class MolecularFormula {
     ArrayList<String> elements=new ArrayList<String>();
-    
+
  public MolecularFormula(String formula)
  {
      for (int i =0; i<formula.length(); i++)
@@ -30,17 +30,22 @@ public class MolecularFormula {
         
         }
      
-          for (String element: elements)
-          {
-              System.out.println(element);
-          }
- 
-              
+        for (String element: elements)
+        {
+            System.out.println(element);
+        }     
  }
 
+ public void MolecularWeight(){
+     double molecularMass = 0;
+     for (int i = 0; i<elements.size(); i++){
+         System.out.println("Element is: " + elements.get(i));
+     }
+ }
+ 
      public static void main (String[] args)
      {
-         MolecularFormula one = new MolecularFormula("H2FE2O");
+         MolecularFormula one = new MolecularFormula("HE2F2OF");
      }
  }
 
