3146271e372efef3bb949564027b87f343e522bd
AFederici <alexanderfederici@hotmail.com>
2017-01-10 20:42:39 -0600

    fix

--- src/edu/IMSA/Intersession/chemistrytools/Element.java
+++ src/edu/IMSA/Intersession/chemistrytools/Element.java
@@ -86,7 +86,8 @@ public class Element {
         symbol = scan.next();
         name = scan.next();
         weight = scan.next();
-        System.out.println("Number: "+ atomicNumb + " Symbol: " + symbol + " Name: " + name + " Weight: " + weight1);
+        System.out.println("Number: "+ atomicNumb + " Symbol: " + symbol + " Name: " + name + " Weight: " + weight);
+        
         
     }
 }
