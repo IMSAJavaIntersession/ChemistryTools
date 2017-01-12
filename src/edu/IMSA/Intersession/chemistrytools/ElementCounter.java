48d57071777d58353ae50457c3e07b131e7d388f
lrdemolition <dr.lrdemolition@gmail.com>
2017-01-11 19:12:58 -0600

    map

--- src/edu/IMSA/Intersession/chemistrytools/ElementCounter.java
+++ src/edu/IMSA/Intersession/chemistrytools/ElementCounter.java
@@ -4,11 +4,21 @@
  * and open the template in the editor.
  */
 package edu.IMSA.Intersession.chemistrytools;
+import java.util.*;
 
 /**
  *
  * @author student
  */
 public class ElementCounter {
-    
+    public static void main(String[] args) {
+      Map m1 = new HashMap(); 
+      m1.put("C", "3");
+      m1.put("B", "2");
+      m1.put("A", "1");
+      System.out.println();
+      System.out.println(" Map Elements");
+      System.out.print("\t" + m1);
+   }
+}
 }
