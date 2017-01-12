8a5421d03536bca504456699a36c7eda318e004d
AX20160802\student <anniexu2001@gmail.com>
2017-01-11 21:03:08 -0600

    add compareMap in element Counter

--- src/edu/IMSA/Intersession/chemistrytools/ElementCounter.java
+++ src/edu/IMSA/Intersession/chemistrytools/ElementCounter.java
@@ -22,6 +22,16 @@ public class ElementCounter {
   //      molecule.getelements
    //     for (Map.Entry<)
     }
+    public Map<Element, String> compareMap(Map<Element, Integer> m1, Map<Element, Integer> map){
+        Map<Element, String> results = new HashMap();
+        for (Map.Entry<Element, Integer> e: m1.entrySet()){
+            Integer val1 = e.getValue();
+            Integer val2 = map.get(e.getKey());
+            String Val = val1 + ":" + (val2 != null ? val2 : 0);
+            results.put(e.getKey(), Val);
+        }
+        return results;
+    }
 }
 
 
